22e3ea46bcd95f1655977414eecbd6653a0ed467
kimsayhi <kimse418@gmail.com>
2024-11-11 19:36:16 +0900

    feat: 유튜브 뮤직 추가

--- src/app/gift/page.js
+++ src/app/gift/page.js
@@ -1,14 +1,18 @@
 "use client";
 import FirstCard from "@/components/FirstCard";
+import LastCard from "@/components/LastCard";
 import MyLetterCard from "@/components/MyLetterCard";
 import PeopleLettersCard from "@/components/PeopleLettersCard";
+import YouTube from "react-youtube";
 
 export default function giftPage() {
   return (
     <div className="items-center  w-full mb-[100px] flex flex-col gap-[100px]">
+      <YouTube videoId="8OYTUFtN3V8" opts={{ playerVars: { autoplay: 1 } }} />
       <FirstCard />
       <MyLetterCard />
       <PeopleLettersCard />
+      <LastCard />
     </div>
   );
 }
---
+++ src/components/LastCard.js
@@ -0,0 +1,24 @@
+import Card from "./Card";
+
+export default function LastCard() {
+  return (
+    <Card>
+      <div className="p-10 font-letter text-lg font-bold">
+        반짝반짝 빛나는 내 이쁜 수현이!
+        <br />
+        항상 웃음 가득한 일만 가득하길 바랄게
+        <br />
+        많은 사람들이 수현이의 생일을 축하해줬으니
+        <br />
+        맛있는거 많이 먹구 <br />
+        행복한 하루 보내자
+        <br />
+        <br />
+        <span className="text-2xl">진심으로 생일 축하해 이쁜이❤️</span>
+        <br />
+        <br />
+        안녕~~
+      </div>
+    </Card>
+  );
+}
